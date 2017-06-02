9efd7e44af74dd86eb231ee2d75847254a0e1836
JosephFleetham <joseph.fleetham@gmail.com>
2017-06-02 20:34:06 +0000

    Styling the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://grammable-joseph-fleetham.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -83,4 +84,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
